b7134649ecf7696facb0e89d31068cadb19609b3
Do Thanh Tuan <tuando@next-aeon.com>
2020-04-24 13:14:04 +0900

    + Demo app for connecting and controlling PivoBasic SdK

---
+++ PivoBasicSDKTestApp/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  RotatorTestApp
+//
+//  Created by Tuan on 2020/02/21.
+//  Copyright © 2020 3i. All rights reserved.
+//
+
+import UIKit
+import PivoBasicSDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    
+    if let vc = SelectDeviceViewController.storyboardInstance() {
+      window?.rootViewController = vc
+      window?.makeKeyAndVisible()
+    }
+    
+    if let licenseFileURL = Bundle.main.url(forResource: "licenseKey", withExtension: "json") {
+      do {
+        try PivoBasicSDK.shared.unlockWithLicenseKey(licenseKeyFileURL: licenseFileURL)
+      }
+      catch {
+        print(error)
+      }
+    }
+    
+    return true
+  }
+
+}
+
