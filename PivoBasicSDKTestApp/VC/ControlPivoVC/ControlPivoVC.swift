b36ddb8329065fa3ed636fb56cd0e07f4c83d772
tass-kwon <tass.kwon@3i.ai>
2026-01-27 13:43:45 +0900

    feat: easing but need clean

--- PivoBasicSDKTestApp/VC/ControlPivoVC/ControlPivoVC.swift
+++ PivoBasicSDKTestApp/VC/ControlPivoVC/ControlPivoVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import PivoBasicSDK
 
 class ControlPivoVC: UIViewController {
-
+  
   static func storyboardInstance() -> ControlPivoVC? {
     let storyboard = UIStoryboard(name: String(describing: ControlPivoVC.self), bundle: nil)
     return storyboard.instantiateInitialViewController() as? ControlPivoVC
@@ -22,10 +22,12 @@ class ControlPivoVC: UIViewController {
   @IBOutlet weak var tfAngle: UITextField!
   @IBOutlet weak var buttonSpeed: UIButton!
   @IBOutlet weak var bypassSwitch: UISwitch!
-    
-    private var oneDegreeRotateLeftCount: Int = 0
-    private var oneDegreeRotateRightCount: Int = 0
-    
+  @IBOutlet weak var easingSwitch: UISwitch!
+  
+  private var oneDegreeRotateLeftCount: Int = 0
+  private var oneDegreeRotateRightCount: Int = 0
+  private var currentSpeed: Int = 12
+
   private lazy var pivoSDK = PivoSDK.shared
   
   override func viewDidLoad() {
@@ -49,11 +51,21 @@ class ControlPivoVC: UIViewController {
     let supportedSpeeds = pivoSDK.getSupportedSpeedsInSecondsPerRound()
     guard supportedSpeeds.count > 0 else { return }
     let maxSpeed = supportedSpeeds[0]
+    currentSpeed = maxSpeed
     buttonSpeed.setTitle("\(maxSpeed) s/r", for: .normal)
     setPivoFastestSpeed()
-    
+
     scrollView.keyboardDismissMode = .onDrag
     didToggleByPassRCChanged(bypassSwitch)
+    updateEasingSwitchState()
+  }
+
+  private func updateEasingSwitchState() {
+    let isSupported = pivoSDK.isEasingSupported()
+    easingSwitch.isEnabled = isSupported
+    if !isSupported {
+      easingSwitch.isOn = false
+    }
   }
   
   private func setPivoFastestSpeed() {
@@ -61,50 +73,57 @@ class ControlPivoVC: UIViewController {
   }
   
   @IBAction func didRotateLeftByDegreeButtonClicked(_ sender: Any) {
-      resetOneDegreeRotateCount()
+    resetOneDegreeRotateCount()
     resignResponder()
-    if let angleStr = tfAngle.text, var angle = Int(angleStr) {
-      angle = angle > 360 ? 360 : angle
-      tfAngle.text = "\(angle)"
-      pivoSDK.turnLeft(angle: angle)
+    guard let angleStr = tfAngle.text, var angle = Int(angleStr) else { return }
+    angle = min(angle, 360)
+    tfAngle.text = "\(angle)"
+
+    if easingSwitch.isOn {
+      let config = EasingConfig(startSpeed: currentSpeed, endSpeed: 510)
+      pivoSDK.turnLeftEasing(angle: Double(angle), config: config)
+    } else {
       do {
         try pivoSDK.turnLeftWithFeedback(angle: angle)
-      }
-      catch {
+      } catch {
         pivoSDK.turnLeft(angle: angle)
       }
     }
   }
   
   @IBAction func didRotateRightByDegreeButtonClicked(_ sender: Any) {
-      resetOneDegreeRotateCount()
+    resetOneDegreeRotateCount()
     resignResponder()
-    if let angleStr = tfAngle.text, var angle = Int(angleStr) {
-      angle = angle > 360 ? 360 : angle
-      tfAngle.text = "\(angle)"
+    guard let angleStr = tfAngle.text, var angle = Int(angleStr) else { return }
+    angle = min(angle, 360)
+    tfAngle.text = "\(angle)"
+
+    if easingSwitch.isOn {
+      let config = EasingConfig(startSpeed: currentSpeed, endSpeed: 510)
+      pivoSDK.turnRightEasing(angle: Double(angle), config: config)
+    } else {
       do {
         try pivoSDK.turnRightWithFeedback(angle: angle)
-      }
-      catch {
+      } catch {
         pivoSDK.turnRight(angle: angle)
       }
     }
   }
   
   @IBAction func didRotateLeftContinouslyButtonClicked(_ sender: Any) {
-      resetOneDegreeRotateCount()
+    resetOneDegreeRotateCount()
     resignResponder()
     pivoSDK.turnLeftContinuously()
   }
   
   @IBAction func didRotateRightContinouslyButtonClicked(_ sender: Any) {
-      resetOneDegreeRotateCount()
+    resetOneDegreeRotateCount()
     resignResponder()
     pivoSDK.turnRightContinuously()
   }
   
   @IBAction func didStopButtonClicked(_ sender: Any) {
-      resetOneDegreeRotateCount()
+    resetOneDegreeRotateCount()
     resignResponder()
     pivoSDK.stop()
   }
@@ -136,35 +155,38 @@ class ControlPivoVC: UIViewController {
     navigationController?.popViewController(animated: true)
   }
   
+  @IBAction func didToggleEasingChanged(_ sender: UISwitch) {
+    labelCommand.text = sender.isOn ? "Easing Mode ON" : "Normal Mode"
+  }
   
   @IBAction func didToggleByPassRCChanged(_ sender: UISwitch) {
     do {
       guard try pivoSDK.isByPassRemoteControllerSupported() else {
         return
       }
-        
+      
       print("bypass is \(sender.isOn)")
       sender.isOn ? pivoSDK.turnOnByPassRemoteController() : pivoSDK.turnOffBypassRemoteController()
-//      sender.isOn ? pivoConnectionByPassRemoteControllerOn() : pivoConnectionByPassRemoteControllerOff()
+      //      sender.isOn ? pivoConnectionByPassRemoteControllerOn() : pivoConnectionByPassRemoteControllerOff()
     }
     catch {
       print(error)
     }
   }
-    @IBAction func didToggleNotifierRCChanged(_ sender: UISwitch) {
-        do {
-            guard try pivoSDK.isNotifierSupported() else {
-                return
-            }
-            
-            print("notifier is \(sender.isOn)")
-            sender.isOn ? pivoSDK.turnOnNotifier() : pivoSDK.turnOffNotifier()
-        }
-        catch {
-            print(error)
-        }
+  @IBAction func didToggleNotifierRCChanged(_ sender: UISwitch) {
+    do {
+      guard try pivoSDK.isNotifierSupported() else {
+        return
+      }
+      
+      print("notifier is \(sender.isOn)")
+      sender.isOn ? pivoSDK.turnOnNotifier() : pivoSDK.turnOffNotifier()
     }
-    
+    catch {
+      print(error)
+    }
+  }
+  
   private func resignResponder() {
     tfAngle.resignFirstResponder()
   }
@@ -180,8 +202,8 @@ extension ControlPivoVC {
     
     let changeAction = UIAlertAction(title: "Change", style: .default) { (_) in
       if let textFields = alertController.textFields,
-        let name = textFields[0].text,
-        !name.isEmpty {
+         let name = textFields[0].text,
+         !name.isEmpty {
         self.pivoSDK.changePivoName(newName: name)
       }
     }
@@ -205,8 +227,10 @@ extension ControlPivoVC {
     
     alert.addPickerView(values: pickerViewValues, initialSelection: pickerViewSelectedValue) { [weak self] vc, picker, index, values in
       guard let strongSelf = self else { return }
-      strongSelf.buttonSpeed.setTitle("\(speeds[index.row]) s/r", for: .normal)
-      strongSelf.pivoSDK.setSpeedBySecondsPerRound(speeds[index.row])
+      let selectedSpeed = speeds[index.row]
+      strongSelf.currentSpeed = selectedSpeed
+      strongSelf.buttonSpeed.setTitle("\(selectedSpeed) s/r", for: .normal)
+      strongSelf.pivoSDK.setSpeedBySecondsPerRound(selectedSpeed)
     }
     
     alert.addAction(title: "Done", style: .cancel)
@@ -220,11 +244,11 @@ extension ControlPivoVC {
       self?.present(alert, animated: true, completion: nil)
     }
   }
-    
-    private func resetOneDegreeRotateCount() {
-        oneDegreeRotateLeftCount = 0
-        oneDegreeRotateRightCount = 0
-    }
+  
+  private func resetOneDegreeRotateCount() {
+    oneDegreeRotateLeftCount = 0
+    oneDegreeRotateRightCount = 0
+  }
 }
 
 extension ControlPivoVC: PivoConnectionDelegate {
@@ -244,29 +268,29 @@ extension ControlPivoVC: PivoConnectionDelegate {
   func pivoConnectionByPassRemoteControllerOff() {
     labelCommand.text = "By Pass Remote Controller Off"
   }
-    
-    func pivoConnectionNotifierOn() {
-        labelCommand.text = "1 Degree Rotate Notifier On"
-    }
-     
-    func pivoConnectionNotifierOff() {
-        labelBatteryLevel.text = "Battery Level:"
-        labelCommand.text = "1 Degree Rotate Notifier turned Off"
-    }
-    
-    func pivoConnectionDidRotate1DegreeLeft() {
-        oneDegreeRotateLeftCount += 1
-        labelBatteryLevel.text = "Rotated 1 Degree Left: \(oneDegreeRotateLeftCount)"
-    }
-    
-    func pivoConnectionDidRotate1DegreeRight() {
-        oneDegreeRotateRightCount += 1
-        labelBatteryLevel.text = "Rotated 1 Degree Right: \(oneDegreeRotateRightCount)"
-    }
+  
+  func pivoConnectionNotifierOn() {
+    labelCommand.text = "1 Degree Rotate Notifier On"
+  }
+  
+  func pivoConnectionNotifierOff() {
+    labelBatteryLevel.text = "Battery Level:"
+    labelCommand.text = "1 Degree Rotate Notifier turned Off"
+  }
+  
+  func pivoConnectionDidRotate1DegreeLeft() {
+    oneDegreeRotateLeftCount += 1
+    labelBatteryLevel.text = "Rotated 1 Degree Left: \(oneDegreeRotateLeftCount)"
+  }
+  
+  func pivoConnectionDidRotate1DegreeRight() {
+    oneDegreeRotateRightCount += 1
+    labelBatteryLevel.text = "Rotated 1 Degree Right: \(oneDegreeRotateRightCount)"
+  }
   
   func pivoConnection(batteryLevel: Int) {
     labelBatteryLevel.text = "Battery Level: \(batteryLevel)%"
-      labelCommand.text = "BATTERY_CHANGED"
+    labelCommand.text = "BATTERY_CHANGED"
   }
   
   func pivoConnection(didDisconnect id: String) {
